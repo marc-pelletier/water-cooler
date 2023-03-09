2b18b05e1346d9281a1b2660832cfd404f30da0c
Steve <marlumpy@gmail.com>
2023-03-09 17:02:37 -0500

    add invite removal

--- controllers/invites.js
+++ controllers/invites.js
@@ -26,7 +26,7 @@ function joinTap(req, res, next) {
         if (!tap.users.join(' - ').includes(req.user._id.toString())) {
             tap.users.push(req.user._id)
             tap.save()
-            let inviteIdx = req.user.tInvitesIn.findIndex(invite => invite == tap.id)
+            let inviteIdx = req.user.tInvitesIn.findIndex(invite => invite.toString() == tap._id.toString())
             req.user.tInvitesIn.splice(inviteIdx, 1)
             req.user.save()
         }
