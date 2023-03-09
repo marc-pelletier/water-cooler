e4b97d4913ca486356463f5ae6e2c1abcf3f06ff
Steve <marlumpy@gmail.com>
2023-03-09 12:48:07 -0500

    final touches before deployment

--- routes/users/index.js
+++ routes/users/index.js
@@ -20,7 +20,8 @@ router.get('/:userid/invite', function(req, res, next) {
     Tap.find({}, (err, taps) => {
         User.findById(req.params.userid, (err, selectedUser) => {
             taps = taps.filter(tap => tap.users.join(' - ').includes(req.user._id.toString()))
-            res.render('users/invite', { user: req.user, taps, selectedTap: null, selectedChannel: null, selectedUser });
+            inviteTaps = taps.filter(tap => !tap.users.join(' - ').includes(req.params.userid))
+            res.render('users/invite', { user: req.user, taps, selectedTap: null, selectedChannel: null, selectedUser, inviteTaps });
         })
     })
 });
@@ -28,8 +29,12 @@ router.get('/:userid/invite', function(req, res, next) {
 router.post('/:userid/invite/:tapid', function(req, res, next) {
     if (!req.user) return res.redirect('/')
     User.findById(req.params.userid, (err, user) => {
-        user.tInvitesIn.push(req.params.tapid)
-        user.save()
+        Tap.findById(req.params.tapid, (err, user) => {
+            if (!tap.users.join(' - ').includes(req.params.userid)) {
+                user.tInvitesIn.push(req.params.tapid)
+                user.save()
+            }
+        })
     })
     res.redirect('/users/'+req.params.userid+'/invite')
 });
